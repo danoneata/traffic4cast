b5926a3a1997a8a7fe428871597666b64ae9cd73
Cosmin-George Alexandru <cosmingeorge.alexandru@gmail.com>
2019-07-02 21:05:14 +0300

    Improve data manipulation by adding a data sample class, Traffic4CastSample, that exposes the path, data, city and date.
    Traffic4CastDataset __getitem__ updated to return an Traffic4CastSample.
    
    Improve code quallity by adding type hints and doc-strings.

--- src/dataset.py
+++ src/dataset.py
@@ -1,16 +1,54 @@
+from typing import List, Callable
 import os
-import operator
-import functools
+import datetime
 
+import numpy as np
 import h5py
 import torch
-import numpy as np
-from torch.utils.data import Dataset
+import torch.utils.data
 
-class Traffic4CastDataset(Dataset):
-    def __init__(self, root_dir, phase, cities=["Berlin", "Istanbul", "Moscow"],
-            transform=[]):
+
+class Traffic4CastSmaple(object):
+    """ Traffic4cast data wrapper.
+
+        Attributes:
+            path (str): Path to the .hdf5 data file.
+            data (torch.tensor): 4D torch.tensor for the sample data.
+            city (str): City where data sample was collected.
+            date (datetime): Date when the data sample was collected.
+    """
+
+    def __init__(self, city: str, path: str):
+        """ Initializes the Traffic4CastSmaple data sample
+
+            Args:
+                city: Name of they city where the data sample was collected.
+                path: Path to the .hdf5 data file.
         """
+
+        self.path = path
+        self.data = None
+        self.city = city
+        self.date = datetime.datetime.strptime(
+            os.path.basename(path).split('_')[0], '%Y%m%d')
+
+    def load(self):
+        """ Load the data sample in the .hdf5 file """
+
+        self.data = torch.from_numpy(
+            np.array(h5py.File(self.path, 'r')['array']))
+
+
+class Traffic4CastDataset(torch.utils.data.Dataset):
+    """ Implementation of the pytorch Dataset. """
+
+    def __init__(self,
+                 root_dir: str,
+                 phase: str,
+                 cities: List[str] = ["Berlin", "Istanbul", "Moscow"],
+                 transform: List[Callable] = []):
+        """ Initializes the Traffic4CastDataset.
+
         Args:
             root_dir (string): Path to the root data directory.
             phase (string): One of: 'training', 'test', 'validation'. Used to
@@ -21,21 +59,46 @@ class Traffic4CastDataset(Dataset):
                 sample.
         """
 
-        self.root_dir = root_dir
-        self.transform = transform
-        self.cities = cities
-        self.files = [f"{root_dir}/{city}/{city}_{phase}/{file}"
-                for city in self.cities
-                for file in os.listdir(f"{root_dir}/{city}/{city}_{phase}/")]
-        self.len = len(self.files)
+        self.transforms = transform
+        self.files = {
+            city: [
+                f"{root_dir}/{city}/{city}_{phase}/{file}"
+                for file in sorted(
+                    os.listdir(f"{root_dir}/{city}/{city}_{phase}/"))
+            ]
+            for city in cities
+        }
+
+        self.len = sum([len(files) for city, files in self.files.items()])
 
     def __len__(self):
+        """ Gets length of the dataset. """
         return self.len
 
-    def __getitem__(self, idx):
-        img = torch.from_numpy(np.array(h5py.File(self.files[idx], 'r')['array']))
-        for transform in self.transform:
-            img = transform(img)
+    def __getitem__(self, idx: int):
+        for city, files in self.files.items():
+            if idx >= len(files):
+                idx = idx - len(files)
+            else:
+                stream = Traffic4CastSmaple(city, files[idx])
+                stream.load()
+                break
 
-        return img
+        for transform in self.transforms:
+            stream.data = transform(stream.data)
 
+        return stream
+
+    @classmethod
+    def collate_list(cls, samples_list: List[Traffic4CastSmaple]
+                    ) -> List[Traffic4CastSmaple]:
+        """ Collates a list of Traffic4CastSmaple.
+
+            Args:
+                samples_list: List of Traffic4CastSmaple samples.
+
+            Return:
+                List[Traffic4CastSmaple]
+        """
+
+        return samples_list
