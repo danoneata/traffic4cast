10f9c6808a2046a29af88a0e2a6c40f73e89a536
Dan Oneata <dan.oneata@gmail.com>
2019-07-18 16:57:59 +0300

    Store in the format required for the leaderboard
    The predictions are now stored as a 5 × 3 × 495 × 436 × 3 tensor; see
    the `write_output_files` functionfrom the official repository:
    
    https://github.com/iarai/NeurIPS2019-traffic4cast/blob/master/utils/create_submissiontest_like.py

--- utils.py
+++ utils.py
@@ -2,17 +2,36 @@ import os
 
 from datetime import datetime
 
+import h5py
 import numpy as np
 
 from src.dataset import Traffic4CastSample
 
 
+def write_data(data, filename):
+    """Write data in gzipped h5 format.
+
+    This function was taken almost verbatim from the official competition repo:
+    https://github.com/iarai/NeurIPS2019-traffic4cast/blob/master/utils/create_submissiontest_like.py#L34-L40
+    """
+    f = h5py.File(filename, 'w', libver='latest')
+    f.create_dataset('array',
+                     shape=data.shape,
+                     data=data,
+                     compression='gzip',
+                     compression_opts=9)
+    f.close()
+
+
 def cache(func, path, to_overwrite, *args, **kwargs):
     if os.path.exists(path) and not to_overwrite:
-        return np.load(path)
+        result = np.array(h5py.File(path, 'r')['array'])
+        result = result.astype(np.uint8)
+        return result
     else:
         result = func(*args, **kwargs)
-        np.save(path, result)
+        results = result.astype(np.uint8)
+        write_data(result, path)
         return result
 
 
