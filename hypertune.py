e91e8ef2bf218da0daf528953488bc75d12993a8
Dan Oneata <dan.oneata@gmail.com>
2019-08-07 14:28:10 +0300

    MINOR Run yapf

--- hypertune.py
+++ hypertune.py
@@ -1,6 +1,6 @@
 import argparse
 import logging
-import os 
+import os
 
 from train import train
 
@@ -11,7 +11,9 @@ import hpbandster.core.nameserver as hpns
 import hpbandster.core.result as hpres
 from hpbandster.optimizers import HyperBand as HyperBand
 
+
 class PyTorchWorker(Worker):
+
     def __init__(self, city, model_type, **kwargs):
         super().__init__(**kwargs)
         # Do init stuff
@@ -19,57 +21,88 @@ class PyTorchWorker(Worker):
         self.model_type = model_type
 
     def compute(self, config, budget, *args, **kwargs):
-        """
-        The input parameter "config" (dictionary) contains the sampled configurations passed by the bohb optimizer
-		"""
+        """ The input parameter "config" (dictionary) contains the sampled
+        configurations passed by the bohb optimizer. """
         return train(self.city, self.model_type, config, max_epochs=budget)
-    
+
     @staticmethod
     def get_configspace():
-        """
-        It builds the configuration space with the needed hyperparameters.
+        """ It builds the configuration space with the needed hyperparameters.
         It is easily possible to implement different types of hyperparameters.
-        Beside float-hyperparameters on a log scale, it is also able to handle categorical input parameter.
+        Beside float-hyperparameters on a log scale, it is also able to handle
+        categorical input parameter.
         :return: ConfigurationsSpace-Object
         """
         cs = CS.ConfigurationSpace()
-        lr = CSH.UniformFloatHyperparameter('optimizer_lr', lower=1e-6, upper=1e-1, default_value='1e-2', log=True)
-    
+        lr = CSH.UniformFloatHyperparameter('optimizer_lr',
+                                            lower=1e-6,
+                                            upper=1e-1,
+                                            default_value='1e-2',
+                                            log=True)
+
         cs.add_hyperparameters([lr])
-        return cs 
+        return cs
+
 
 def main():
-    parser = argparse.ArgumentParser(description='Parallel execution of hyper-tuning')
-    parser.add_argument('--min_budget',   type=float, help='Minimum budget used during the optimization.',    default=1)
-    parser.add_argument('--max_budget',   type=float, help='Maximum budget used during the optimization.',    default=10)
-    parser.add_argument('--n_iterations', type=int,   help='Number of iterations performed by the optimizer', default=1)
-    parser.add_argument('--n_workers', type=int,   help='Number of workers to run in parallel.', default=3)
-    parser.add_argument('--worker', help='Flag to turn this into a worker process', action='store_true')
-    parser.add_argument('--shared_directory',type=str, help='A directory that is accessible for all processes, e.g. a NFS share.', default='./results')
-    args=parser.parse_args()
+    parser = argparse.ArgumentParser(
+        description='Parallel execution of hyper-tuning')
+    parser.add_argument('--min_budget',
+                        type=float,
+                        help='Minimum budget used during the optimization.',
+                        default=1)
+    parser.add_argument('--max_budget',
+                        type=float,
+                        help='Maximum budget used during the optimization.',
+                        default=10)
+    parser.add_argument('--n_iterations',
+                        type=int,
+                        help='Number of iterations performed by the optimizer',
+                        default=1)
+    parser.add_argument('--n_workers',
+                        type=int,
+                        help='Number of workers to run in parallel.',
+                        default=3)
+    parser.add_argument('--worker',
+                        help='Flag to turn this into a worker process',
+                        action='store_true')
+    parser.add_argument('--shared_directory',
+                        type=str,
+                        help=('A directory that is accessible '
+                              'for all processes, e.g. a NFS share.'),
+                        default='./results')
+    args = parser.parse_args()
 
-    logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"), 
-                        format='%(asctime)s %(message)s', datefmt='%I:%M:%S')
+    logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"),
+                        format='%(asctime)s %(message)s',
+                        datefmt='%I:%M:%S')
 
     if args.worker:
         # Start a worker in listening mode (waiting for jobs from master)
-        w = PyTorchWorker("Berlin","temporal-regression-speed-12",
-                            run_id="t4cast",id=0, nameserver='127.0.0.1')
+        w = PyTorchWorker("Berlin",
+                          "temporal-regression-speed-12",
+                          run_id="t4cast",
+                          id=0,
+                          nameserver='127.0.0.1')
         w.run(background=False)
         exit(0)
 
-    result_logger = hpres.json_result_logger(directory=args.shared_directory, overwrite=True)
+    result_logger = hpres.json_result_logger(directory=args.shared_directory,
+                                             overwrite=True)
 
-    # Start a nameserver 
+    # Start a nameserver
     NS = hpns.NameServer(run_id='t4cast', host='127.0.0.1', port=None)
     NS.start()
 
     # Run and optimizer
-    bohb = HyperBand(configspace = PyTorchWorker.get_configspace(), # model can be an arg here?
-                        run_id = 't4cast',
-                        result_logger=result_logger,
-                        eta = 3,
-                        min_budget=args.min_budget, max_budget=args.max_budget)    
+    bohb = HyperBand(
+        configspace=PyTorchWorker.get_configspace(
+        ),  # model can be an arg here?
+        run_id='t4cast',
+        result_logger=result_logger,
+        eta=3,
+        min_budget=args.min_budget,
+        max_budget=args.max_budget)
 
     res = bohb.run(n_iterations=args.n_iterations, min_n_workers=args.n_workers)
 
@@ -83,11 +116,15 @@ def main():
     inc_run = inc_runs[-1]
     all_runs = res.get_all_runs()
 
-    print("\nBEST loss {:6.2f}".format(1-inc_run.loss))
-    print('A total of %i unique configurations where sampled.' % len(id2config.keys()))
+    print("\nBEST loss {:6.2f}".format(1 - inc_run.loss))
+    print('A total of %i unique configurations where sampled.' %
+          len(id2config.keys()))
     print('A total of %i runs where executed.' % len(all_runs))
-    print('Total budget corresponds to %.1f full function evaluations.'%(sum([r.budget for r in all_runs])/args.max_budget))
-    print('The run took  %.1f seconds to complete.'%(all_runs[-1].time_stamps['finished'] - all_runs[0].time_stamps['started']))
+    print('Total budget corresponds to %.1f full function evaluations.' %
+          (sum([r.budget for r in all_runs]) / args.max_budget))
+    print('The run took  %.1f seconds to complete.' %
+          (all_runs[-1].time_stamps['finished'] -
+           all_runs[0].time_stamps['started']))
 
     # cs = worker.get_configspace()
     # config = cs.sample_configuration().get_dictionary()
@@ -95,7 +132,6 @@ def main():
     # res = worker.compute(config=config, budget=1)
     # print(res)
 
+
 if __name__ == "__main__":
     main()
-
-
--- hypertune_results.py
+++ hypertune_results.py
@@ -5,97 +5,112 @@ import plotly
 import plotly.graph_objs as go
 
 import hpbandster.core.result as hpres
-from hypertune import PyTorchWorker as worker 
+from hypertune import PyTorchWorker as worker
 
-np.set_printoptions(precision=3 , floatmode='fixed')
+np.set_printoptions(precision=3, floatmode='fixed')
 np.set_printoptions(suppress=True)
 np.core.arrayprint._line_width = 80
 
-def get_values(runs,id2config):
-	"""
+
+def get_values(runs, id2config):
+    """
 		Parse values from hyper-parameter results to lists. 
 		Needed for parallel coordinates plot.
 	"""
-	values = {}
-	values["loss"] = []
-	max_loss = 99999
+    values = {}
+    values["loss"] = []
+    max_loss = 99999
 
-	for run in runs:
-		if run.loss is None:    
-			run.loss = max_loss
-		values["loss"].append(run.loss)
+    for run in runs:
+        if run.loss is None:
+            run.loss = max_loss
+        values["loss"].append(run.loss)
 
-		config = id2config[run.config_id]['config']
-		
-		for param in config:
-			if not param in values:
-				values[param] = []
-			values[param].append(config[param])
+        config = id2config[run.config_id]['config']
 
-	return values 
+        for param in config:
+            if not param in values:
+                values[param] = []
+            values[param].append(config[param])
+
+    return values
+
 
 def data_par_coords(values, columns):
-	config_space = worker.get_configspace()
-	# print(config_space)
-	# print(config_space.get_hyperparameter("optimizer_lr"))
+    config_space = worker.get_configspace()
+    # print(config_space)
+    # print(config_space.get_hyperparameter("optimizer_lr"))
 
-	KWARGS = {
-		"optimizer_lr": 	{"label":'Learning rate', "range": [1e-6, 1e-1]},
-		"loss":				{"label":'Loss'}
-	}
+    KWARGS = {
+        "optimizer_lr": {
+            "label": 'Learning rate',
+            "range": [1e-6, 1e-1]
+        },
+        "loss": {
+            "label": 'Loss'
+        }
+    }
 
-	data = []
-	for col in columns:
-		try:
-			datum = {"values": values[col]}
-			datum.update(KWARGS[col])
-			data.append(datum)
-		except KeyError:
-			pass
-	return data
+    data = []
+    for col in columns:
+        try:
+            datum = {"values": values[col]}
+            datum.update(KWARGS[col])
+            data.append(datum)
+        except KeyError:
+            pass
+    return data
+
 
 def main():
-	"""
+    """
 		load data from dir
 		print some stuff
 		generate and save par plot into temp-plot.html
 	"""
-	parser = argparse.ArgumentParser(description='Plotting hyper-parameter tuning results')
-	parser.add_argument('--dir',type=str, help='A directory to load results from')
-	args=parser.parse_args()
+    parser = argparse.ArgumentParser(
+        description='Plotting hyper-parameter tuning results')
+    parser.add_argument('--dir',
+                        type=str,
+                        help='A directory to load results from')
+    args = parser.parse_args()
 
-	if args.dir is None:
-		print("usage: python hypetune_results.py --dir DIR")
-		return 
+    if args.dir is None:
+        print("usage: python hypetune_results.py --dir DIR")
+        return
 
-	res = hpres.logged_results_to_HBS_result("./"+args.dir)
-	id2config = res.get_id2config_mapping()
-	incumbent = res.get_incumbent_id()
-	# print(id2config[incumbent])
-	all_runs = res.get_all_runs()
-	print(all_runs[0])
+    res = hpres.logged_results_to_HBS_result("./" + args.dir)
+    id2config = res.get_id2config_mapping()
+    incumbent = res.get_incumbent_id()
+    # print(id2config[incumbent])
+    all_runs = res.get_all_runs()
+    print(all_runs[0])
 
-	print('A total of %i unique configurations where sampled.' % len(id2config.keys()))
-	print('A total of %i runs where executed.' % len(all_runs))
+    print('A total of %i unique configurations where sampled.' %
+          len(id2config.keys()))
+    print('A total of %i runs where executed.' % len(all_runs))
 
-	values = get_values(all_runs,id2config)
-	all_columns = ['loss','optimizer_lr', 'blabla']
-	data = data_par_coords(values,all_columns)
+    values = get_values(all_runs, id2config)
+    all_columns = ['loss', 'optimizer_lr', 'blabla']
+    data = data_par_coords(values, all_columns)
 
-	parcoord_data = [
-		go.Parcoords(
-				line = dict(color = 'blue'
-									# colorscale = 'Jet',
-									# showscale = True,
-									# reversescale = True,
-									# cmin = 0,
-									# cmax = 1
-									),
-				labelfont = dict(size=20),
-				tickfont = dict(size=18),
-				dimensions = list(data))]
+    parcoord_data = [
+        go.Parcoords(
+            line=dict(
+                color='blue'
+                # colorscale = 'Jet',
+                # showscale = True,
+                # reversescale = True,
+                # cmin = 0,
+                # cmax = 1
+            ),
+            labelfont=dict(size=20),
+            tickfont=dict(size=18),
+            dimensions=list(data))
+    ]
 
-	plotly.offline.plot(parcoord_data,  auto_open=False)
+    plotly.offline.plot(parcoord_data, auto_open=False)
+
 
 if __name__ == "__main__":
-	main()
+    main()
