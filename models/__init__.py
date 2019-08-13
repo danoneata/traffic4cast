5204b89da83cd16941281c89f9fb19dbc85b630f
Dan Oneata <dan.oneata@gmail.com>
2019-08-13 10:11:45 +0300

    Work in progress

--- models/__init__.py
+++ models/__init__.py
@@ -5,4 +5,5 @@ MODELS = {
     "naive": models.baseline.Naive,
     "zeros": models.baseline.Zeros,
     "single-channel-temporal-regression-12": lambda: models.nn.Temporal(12, 1, 1, models.nn.TemporalRegression(12)),
+    "seasonal-temporal-regression": lambda: models.nn.TemporalDate(12, 1, 1, models.nn.SeasonalTemporalRegression(12)),
 }
--- models/nn.py
+++ models/nn.py
@@ -1,6 +1,8 @@
 from typing import List, Union, Dict
+
 import collections
 import enum
+import pdb
 
 import numpy as np
 import torch
@@ -155,6 +157,136 @@ class Temporal(torch_nn.Module):
                     yield minibatch
 
 
+class TemporalDate(torch_nn.Module):
+    """ Temporal model for a single single channel. Predicts the next value by
+    considering a fixed history at the same coordinate. """
+
+    def __init__(self, past: int, future: int, num_channels: int,
+                 module: Union[torch_nn.Module, torch_nn.Sequential]):
+        super(TemporalDate, self).__init__()
+        self.past = past
+        self.future = future
+        self.num_channels = num_channels
+        self.add_module('module', module)
+
+    def load(self, path):
+        self.load_state_dict(torch.load(path))
+
+    def forward(self, input):
+        return self.module(input)
+
+    def predict(self,
+                frames: List[int],
+                sample: src.dataset.Traffic4CastSample,
+                mode: ReplacementMode = ReplacementMode.ALL_FROM_PREDICTED
+               ) -> Dict[int, torch.tensor]:
+        """ Predict requested frames.
+
+            Predicts the requested frames for the given sample. For each frame
+            to predict the input frames are replaced with predicted frames
+            based on the selected replacement mode. The default replacement
+            mode is ReplacementMode.ALL_FROM_PREDICTED
+
+            Args:
+                frames: Frames for which to generate predictions.
+                sample: Sample for which to generate predictions.
+                mode: Replacement mode.
+
+            Return:
+                Dictionary map of frames indices to predicted frames(tensors).
+
+            Reaise:
+                Exception: When the any frames necessary to predict the current
+                    frame are invaild and they cannot be replaced with already
+                    predicted frames.
+        """
+        predictions = collections.OrderedDict.fromkeys(frames)
+        self.eval()
+        with torch.no_grad():
+            for frame, (slice, valid) in zip(
+                    frames, sample.temporal_slices(self.past, frames, True)):
+
+                # Don't predict again if already predicted.
+                if predictions[frame] is not None:
+                    continue
+
+                if mode == ReplacementMode.ALL_FROM_PREDICTED:
+                    for v, f in enumerate(range(frame - self.past, frame)):
+                        if (f in predictions.keys() and
+                                predictions[f] is not None):
+                            slice[v] = predictions[f]
+                            valid[v] = 1
+                    if not valid.all():
+                        raise Exception(f"Invalid frames for slice {f}.")
+                elif mode == ReplacementMode.INVALID_FROM_PREDICTED:
+                    if not valid.all():
+                        for v, f in enumerate(range(frame - self.past, frame)):
+                            if not valid[v]:
+                                if (f in predictions.keys() and
+                                        predictions[f] is not None):
+                                    slice[v] = predictions[f]
+                                else:
+                                    raise Exception(
+                                        f"Invalid frames for slice {f}.")
+                else:
+                    raise Exeception(f"Invalid frame repacement mode.")
+
+                pred_slice = self(
+                    slice.reshape(-1, slice.shape[2],
+                                  slice.shape[3]).unsqueeze(0)).squeeze(0)
+                for pred_frame in range(pred_slice.shape[0] //
+                                        self.num_channels):
+                    predictions[frame +
+                                pred_frame] = pred_slice[pred_frame *
+                                                         self.num_channels:
+                                                         (pred_frame + 1) *
+                                                         self.num_channels]
+
+            # The model might predict other frames then the requested ones.
+            # Remove from the result the frames that were not requested.
+            for key in predictions.keys():
+                if key not in frames:
+                    del predictions[key]
+
+        return predictions
+
+    def ignite_batch(self, batch, device, non_blocking):
+        batch1, date = batch
+        batch1 = batch1.to(device)
+        batch1 = batch1.reshape(
+            batch1.shape[0],
+            -1,
+            batch1.shape[3],
+            batch1.shape[4],
+        )
+        inp = batch1[:, :self.past * self.num_channels], date
+        tgt = batch1[:, self.past * self.num_channels:]
+        return inp, tgt
+
+    def ignite_random(self, loader, num_minibatches, minibatch_size,
+                      epoch_fraction):
+        num_batches = int(len(loader) * epoch_fraction)
+        for batch in loader:
+            for sample in batch:
+                for minibatch in sample.random_temporal_batches(
+                        num_minibatches, minibatch_size,
+                        self.past + self.future):
+                    yield minibatch, sample.date
+            num_batches -= 1
+            if num_batches <= 0:
+                return
+
+    def ignite_all(self, loader, minibatch_size):
+        for batch in loader:
+            for sample in batch:
+                num_frames = sample.data.shape[0]
+                for minibatch in sample.selected_temporal_batches(
+                        minibatch_size, self.past + self.future,
+                        range(self.past + self.future,
+                              num_frames - self.past - self.future)):
+                    yield minibatch, sample.date
+
+
 class TemporalRegression(torch_nn.Module):
     """ Simple 1 frame 1 channel model. """
 
@@ -173,4 +305,50 @@ class TemporalRegression(torch_nn.Module):
         x = torch.relu(x)
         x = self.conv3(x)
         x = torch.sigmoid(x)
+        return x
+
+
+class SeasonalTemporalRegression(torch_nn.Module):
+    def __init__(self, history: int):
+        super(SeasonalTemporalRegression, self).__init__()
+        self.history = history
+        kwargs = dict(kernel_size=1, stride=1, padding=0, bias=True)
+        self.temp_regr = torch_nn.Sequential(
+            torch_nn.Conv2d(history, 16, **kwargs),
+            torch_nn.ReLU(),
+            torch_nn.Conv2d(16, 16, **kwargs),
+            torch_nn.ReLU(),
+            torch_nn.Conv2d(16, 1, **kwargs),
+        )
+        self.bias_day = torch_nn.Parameter(torch.zeros(7))
+
+    def forward(self, x_date):
+        x, date = x_date 
+        x = self.temp_regr(x)
+        d = date.weekday()
+        x = torch.sigmoid(x + self.bias_day[d])
+        return x
+
+
+class SeasonalTemporalRegressionHeading(torch_nn.Module):
+    def __init__(self, history: int):
+        super(SeasonalTemporalRegression, self).__init__()
+        self.history = history
+        kwargs = dict(kernel_size=1, stride=1, padding=0, bias=True)
+        self.temp_regr = torch_nn.Sequential(
+            torch_nn.Conv2d(history, 16, **kwargs),
+            torch_nn.ReLU(),
+            torch_nn.Conv2d(16, 16, **kwargs),
+            torch_nn.ReLU(),
+            torch_nn.Conv2d(16, 5, **kwargs),
+        )
+        self.bias_day = torch_nn.Parameter(torch.zeros(7, 5))
+        self.v = torch.tensor(v)
+
+    def forward(self, x_date):
+        x, date = x_date 
+        x = self.temp_regr(x)
+        d = date.weekday()
+        x = torch.softmax(x + self.bias_day[d])
+        x = (x * self.v).sum(dim=1, keepdim=True)
         return x
